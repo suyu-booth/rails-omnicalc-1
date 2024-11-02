bcc4b18f45e7de4ddb2784eb4cd95a41806b7c7d
suyu-booth <suyu@chicagobooth.edu>
2024-11-02 01:38:19 +0000

    test1

---
+++ app/models/calc_controller.rb
@@ -0,0 +1,47 @@
+class CalcController < ApplicationController
+  def add_form
+    render({ :template => "calc_templates/add1" })
+  end
+
+  def add_result
+    @sq_new = params[:number]
+    @sq_res = @sq_new.to_f**2
+    render({ :template => "calc_templates/add2" })
+  end
+  def sub_form
+    render({ :template => "calc_templates/sub1" })
+  end
+
+  def sub_result
+    @sqrt_new = params[:user_number]
+    @sqrt_res = @sqrt_new.to_f**0.5
+    render({ :template => "calc_templates/sub2" })
+  end
+  def mult_form
+    render({ :template => "calc_templates/mult1" })
+  end
+
+  def mult_result
+    @years = params[:user_years]
+    @apr = params[:user_apr].to_f
+    @pv = params[:user_pv].to_f
+  
+    @numerator = @apr/100/12 * @pv
+    @denominator =  1- (1+@apr/100/12)**(-(@years.to_f)*12)
+    render({ :template => "calc_templates/mult2" })
+  end
+
+  def div_form
+    render({ :template => "calc_templates/div1" })
+  end
+
+  def div_result
+    @mini = params[:user_min]
+    @maxi = params[:user_max]
+  
+    @randi = rand(@mini.to_f..@maxi.to_f)
+    render({ :template => "calc_templates/div2" })
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
+  get("/") do 
+    redirect("/square/new")
+  end 
   
+  get("/square/new", {
+    :controller: "calc",
+    :action => "add_form"
+  })
+  get("/square/results", {
+    :controller: "calc",
+    :action => "add_result"
+  })
+  get("/square_root/new", {
+    :controller: "calc",
+    :action => "sub_form"
+  })
+  get("/square_root/results", {
+    :controller: "calc",
+    :action => "sub_result"
+  })
+  get("/payment/new", {
+    :controller: "calc",
+    :action => "mult_form"
+  })
+  get("/payment/results", {
+    :controller: "calc",
+    :action => "mult_result"
+  })
+  get("/random/new", {
+    :controller: "calc",
+    :action => "div_form"
+  })
+  get("/random/results", {
+    :controller: "calc",
+    :action => "div_result"
+  })
 end
